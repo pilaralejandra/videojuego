e295b3ce5273d3c0e0b0701d17bc8cf141b70f68
Alejandra Izquierdo <pilarale95@gmail.com>
2018-09-20 17:09:25 -0500

    Testing my code to understand and changing bounce and gravity values

--- js/main.js
+++ js/main.js
@@ -18,9 +18,9 @@ var score = 0;
 var scoreText;
 
 function create() {
-
+    //Add random stars
     //game.add.sprite((Math.random()*800)+1, (Math.random()*600)+1, 'star');
-	
+
 	//  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -50,11 +50,11 @@ function create() {
     ledge = platforms.create(-150, 250, 'ground');
 
     ledge.body.immovable = true;
-	
+	//Here I can create another ledge
 	//ledge = platforms.create(200, 300, 'ground');
 	//ledge.scale.setTo(.5, 2);
     //ledge.body.immovable = true;
-	
+
 	// The player and its settings
     player = game.add.sprite(32, game.world.height - 150, 'dude');
 
@@ -62,15 +62,15 @@ function create() {
     game.physics.arcade.enable(player);
 
     //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
+    player.body.bounce.y = 0.5;
+    player.body.gravity.y = 500;
     player.body.collideWorldBounds = true;
 
     //  Our two animations, walking left and right.
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
-	
-	
+
+
 	 //  Finally some stars to collect
     stars = game.add.group();
 
@@ -89,7 +89,7 @@ function create() {
         //  This just gives each star a slightly random bounce value
         star.body.bounce.y = 0.7 + Math.random() * 0.2;
     }
-	
+
 	//  The score
     scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 	//  Our controls.
@@ -99,12 +99,12 @@ function create() {
 
 function update() {
 	//  Collide the player and the stars with the platforms
-    game.physics.arcade.collide(player, platforms);
+  var hitPlatform =  game.physics.arcade.collide(player, platforms);
 	game.physics.arcade.collide(stars, platforms);
-	
+
 	//  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
     game.physics.arcade.overlap(player, stars, collectStar, null, this);
-	
+
 	//  Reset the players velocity (movement)
     player.body.velocity.x = 0;
 
@@ -129,19 +129,19 @@ function update() {
 
         player.frame = 4;
     }
-    //&& player.body.touching.down
+    //&& player.body.touching.down && hitPlatform
     //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown )
+    if (cursors.up.isDown)
     {
         player.body.velocity.y = -350;
     }
 }
 
 function collectStar (player, star) {
-    
+
     // Removes the star from the screen
     star.kill();
-	
+
 	//  Add and update the score
     score += 10;
     scoreText.text = 'Score: ' + score;
